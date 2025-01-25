4688e901d070c27687173bd03854e24682646a59
Monster0ys <sobolev260403@gmail.com>
2025-01-25 23:09:41 +0300

    Snake reworking

--- snake.py
+++ snake.py
@@ -1,8 +1,18 @@
 import pygame
 pygame.init()
 
+class Snake_stick():
+    def __init__(self,x:int,y:int,horizontal:bool=True):
+        self.x:int=x
+        self.y:int=y
+        self.horizontal:bool=horizontal
 W = 500
 H = 500
+w_snake=4
+lines:list[Snake_stick]=[]
+for i in range(50):
+    lines.append(Snake_stick(W//2-i, H//2, False))
+
 sc=pygame.display.set_mode((W, H))
 WHITE=(255, 255, 255)
 BLUE=(0,0,255)
@@ -10,10 +20,9 @@ BLUE=(0,0,255)
 FPS=60
 clock=pygame.time.Clock()
 
-x = W // 2
-y = H // 2
 speed=1
 direction ="RIGHT"
+
 
 while True:
     for event in pygame.event.get():
@@ -22,24 +31,62 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and direction != "RIGHT":
                 direction="LEFT"
+                for i in range(w_snake):
+                    lines[i].x=lines[w_snake].x-w_snake+i
+                    lines[i].y=lines[w_snake].y
+                    lines[w_snake+1+i].x=lines[w_snake].x+1+i
+                    lines[w_snake+1+i].y=lines[w_snake].y
+                    lines[i].horizontal=False
+                    lines[w_snake+1+i].horizontal=False
+                lines[w_snake].horizontal=False
             elif event.key == pygame.K_RIGHT and direction != "LEFT":
                 direction="RIGHT"
+                for i in range(w_snake):
+                    lines[i].x=lines[w_snake].x+w_snake-i
+                    lines[i].y=lines[w_snake].y
+                    lines[w_snake+1+i].x=lines[w_snake].x-1-i
+                    lines[w_snake+1+i].y=lines[w_snake].y
+                    lines[i].horizontal=False
+                    lines[w_snake+1+i].horizontal=False
+                lines[w_snake].horizontal=False
             elif event.key == pygame.K_UP and direction != "DOWN":
                 direction="UP"
+                for i in range(w_snake):
+                    lines[i].x=lines[w_snake].x
+                    lines[i].y=lines[w_snake].y-w_snake+i
+                    lines[w_snake+1+i].x=lines[w_snake].x
+                    lines[w_snake+1+i].y=lines[w_snake].y+1+i
+                    lines[i].horizontal=True
+                    lines[w_snake+1+i].horizontal=True
+                lines[w_snake].horizontal=True
             elif event.key == pygame.K_DOWN and direction != "UP":
                 direction="DOWN"
+                for i in range(w_snake):
+                    lines[i].x=lines[w_snake].x
+                    lines[i].y=lines[w_snake].y+w_snake-i
+                    lines[w_snake+1+i].x=lines[w_snake].x
+                    lines[w_snake+1+i].y=lines[w_snake].y-1-i
+                    lines[i].horizontal=True
+                    lines[w_snake+1+i].horizontal=True
+                lines[w_snake].horizontal=True
 
     if direction == "LEFT":
-        x=(x-speed+W)%W
+        lines.insert(0,Snake_stick(lines[0].x-1,lines[0].y, False))
     elif direction =="RIGHT":
-        x=(x+speed+W)%W
+        lines.insert(0,Snake_stick(lines[0].x+1,lines[0].y, False))
     elif direction =="UP":
-        y=(y-speed+H)%H
+        lines.insert(0,Snake_stick(lines[0].x,lines[0].y-1, True))
     elif direction =="DOWN":
-        y=(y+speed+H)%H
+        lines.insert(0,Snake_stick(lines[0].x,lines[0].y+1, True))
+    lines.pop()
 
     sc.fill(WHITE)
-    pygame.draw.rect(sc, BLUE, (x, y, 15, 15))
+    for i in range(len(lines)):
+        if lines[i].horizontal:
+            pygame.draw.line(sc, BLUE, [lines[i].x+w_snake, lines[i].y], [lines[i].x-w_snake, lines[i].y])
+        else:
+            pygame.draw.line(sc, BLUE, [lines[i].x, lines[i].y+w_snake], [lines[i].x, lines[i].y-w_snake])
+    # pygame.draw.rect(sc, BLUE, (x, y, 15, 15))
     pygame.display.update()
 
     clock.tick(FPS)
